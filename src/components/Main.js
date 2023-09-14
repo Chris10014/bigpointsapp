9838c210c23ba71b38c99e0bbc4531d2446f6d0d
Christoph Lansche <christoph.lansche@t-online.de>
2023-09-14 14:38:34 +0200

    Removed changes for race input form 	modified:   src/components/Main.js 	deleted:    src/components/RaceInputForm.js

--- src/components/Main.js
+++ src/components/Main.js
@@ -4,11 +4,7 @@ import Home from "./Home";
 import Missing from "./Missing";
 import SportEventsList from "./SportEventsList";
 import SportEventDetails from "./SportEventDetails";
-<<<<<<< HEAD
 import NewSportEvent from "./NewSportEvent";
-=======
-import AddRaceForm from "./AddRaceForm";
->>>>>>> cf9ff316d4e35b75186f543c1794e6221ed29456
 
 const  Main = () => {
 
@@ -21,7 +17,6 @@ const  Main = () => {
           <Route path=":sportEventId/:eventDateId" element={<SportEventDetails />} />
           <Route path="create" element={<NewSportEvent />} />
         </Route>
-        <Route path="addRaces" element={<AddRaceForm />} />
         <Route path="*" element={<Missing />} />
       </Route>
     </Routes>
