bb21b96bbeab89a57158939832eb06eea8cbaf8a
Christoph Lansche <christoph.lansche@t-online.de>
2023-09-14 14:33:47 +0200

    Solve merge conflicts after pulling race-form 	new file:   .vscode/launch.json 	modified:   package-lock.json 	modified:   package.json 	new file:   src/components/AddRaceForm.js 	modified:   src/components/Footer.js 	modified:   src/components/Main.js 	new file:   src/components/RaceInputForm.js

--- src/components/Main.js
+++ src/components/Main.js
@@ -4,7 +4,11 @@ import Home from "./Home";
 import Missing from "./Missing";
 import SportEventsList from "./SportEventsList";
 import SportEventDetails from "./SportEventDetails";
+<<<<<<< HEAD
 import NewSportEvent from "./NewSportEvent";
+=======
+import AddRaceForm from "./AddRaceForm";
+>>>>>>> cf9ff316d4e35b75186f543c1794e6221ed29456
 
 const  Main = () => {
 
@@ -17,6 +21,7 @@ const  Main = () => {
           <Route path=":sportEventId/:eventDateId" element={<SportEventDetails />} />
           <Route path="create" element={<NewSportEvent />} />
         </Route>
+        <Route path="addRaces" element={<AddRaceForm />} />
         <Route path="*" element={<Missing />} />
       </Route>
     </Routes>
